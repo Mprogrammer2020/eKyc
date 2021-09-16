a159409354561423afd286f9348476a683f37a89
Pavneet Singh <pavneet.singh@netsetsoftware.com>
2021-09-16 12:23:24 +0530

    Removed Income Statement

--- eKyc/src/main/java/com/library/ekycnetset/presenter/BaseCheckPresenter.kt
+++ eKyc/src/main/java/com/library/ekycnetset/presenter/BaseCheckPresenter.kt
@@ -423,7 +423,7 @@ class BaseCheckPresenter(
         )
         jsonObject.put("phone2", "")
         jsonObject.put("gender", gender)
-        jsonObject.put("Occupation", viewDataBinding.twoStep.occTxt.text.toString())
+        jsonObject.put("occupation", viewDataBinding.twoStep.occTxt.text.toString())
         jsonObject.put("birthday_day", birthdayDay!!)
         jsonObject.put("birthday_month", birthdayMonth!!)
         jsonObject.put("birthday_year", birthdayYear!!)
